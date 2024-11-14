38214fa28f0e8ecd37375ea0b929af9af9b8c05a
mengoale <alessandro.mengoli9@gmail.com>
2024-11-14 18:38:47 +0100

    Create simple app for structured logging!

---
+++ TutorialNetStructuredLog/PizzaApp.StructuredLogging/Pizza.cs
@@ -0,0 +1,15 @@
+﻿public class Pizza
+{
+   public string Name { get; }
+   public decimal Price { get; }
+   public List<string> Toppings { get; } = new();
+
+   public Pizza(string name, decimal price)
+   {
+      Name = name;
+      Price = price;
+   }
+
+   public void AddTopping(string topping) => Toppings.Add(topping);
+   public void Bake() => Thread.Sleep(1000); // Simulate baking
+}
--- TutorialNetStructuredLog/PizzaApp.StructuredLogging/Program.cs
+++ TutorialNetStructuredLog/PizzaApp.StructuredLogging/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                   builder.AddConsole();
+                   builder.SetMinimumLevel(LogLevel.Debug);
+                })
+                .BuildServiceProvider();
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Application started");
+
+try
+{
+   var pizza = new Pizza("Margherita", 12.99m);
+   logger.LogInformation("Created new pizza {PizzaName} with price {Price}", pizza.Name, pizza.Price);
+
+   pizza.AddTopping("Mozzarella");
+   logger.LogInformation("Added topping {Topping} to pizza {PizzaName}", "Mozzarella", pizza.Name);
+
+   pizza.Bake();
+   logger.LogInformation("Pizza {PizzaName} is baking", pizza.Name);
+}
+catch (Exception ex)
+{
+   logger.LogError(ex, "An error occurred while processing the pizza");
+}
+
+logger.LogInformation("Application ended");
