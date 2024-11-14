1c3813d910fb6a3e0a7cd5b958fb3a56033ec907
mengoale <alessandro.mengoli9@gmail.com>
2024-11-14 18:32:36 +0100

    Created consoleapp for unstructured, structured and serilog logging

---
+++ TutorialNetStructuredLog/PizzaApp.UnstructuredLogging/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine($"[{DateTime.Now}] Application started");
+
+try
+{
+   var pizza = new Pizza("Margherita", 12.99m);
+   Console.WriteLine($"[{DateTime.Now}] Created new pizza: {pizza.Name}");
+
+   pizza.AddTopping("Pomodoro");
+   Console.WriteLine($"[{DateTime.Now}] Added topping to {pizza.Name}");
+
+   pizza.AddTopping("Mozzarella");
+   Console.WriteLine($"[{DateTime.Now}] Added topping to {pizza.Name}");
+
+   pizza.Bake();
+   Console.WriteLine($"[{DateTime.Now}] Pizza {pizza.Name} is baking");
+}
+catch (Exception ex)
+{
+   Console.WriteLine($"[{DateTime.Now}] ERROR: {ex.Message}");
+}
+
+Console.WriteLine($"[{DateTime.Now}] Application ended");
