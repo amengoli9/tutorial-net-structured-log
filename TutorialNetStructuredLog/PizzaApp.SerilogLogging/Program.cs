ea7d052c33a7cbcb6a8843c1b247cdcdc6500061
mengoale <alessandro.mengoli9@gmail.com>
2024-11-14 18:42:07 +0100

    Create simple app for serilog logging

--- TutorialNetStructuredLog/PizzaApp.SerilogLogging/Program.cs
+++ TutorialNetStructuredLog/PizzaApp.SerilogLogging/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/pizza.txt", rollingInterval: RollingInterval.Day)
+                .Enrich.WithThreadId()
+                .Enrich.WithEnvironmentUserName()
+                .CreateLogger();
+
+Log.Information("Application started");
+
+try
+{
+   var pizza = new Pizza("Margherita", 12.99m);
+   Log.Information("Created new pizza {PizzaName} with price {Price:C}", pizza.Name, pizza.Price);
+
+   string topping = "Pomodoro";
+   pizza.AddTopping("Mozzarella");
+   Log.Information("Added topping {Topping} to {@Pizza}", "Mozzarella", topping, pizza);
+
+   topping = "Mozzarella";
+   pizza.AddTopping("Mozzarella");
+   Log.Information("Added topping {Topping} to {@Pizza}", "Mozzarella", topping, pizza);
+
+   pizza.Bake();
+   Log.Information("Pizza {@Pizza} is baking", pizza);
+}
+catch (Exception ex)
+{
+   Log.Error(ex, "An error occurred while processing the pizza");
+}
+finally
+{
+   Log.CloseAndFlush();
+}
